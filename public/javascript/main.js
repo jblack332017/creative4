caed829e6961f4608f3ed95b7c6c9938e4ed4c16
Josh Black <joshblack@tigger.local>
2017-03-15 17:23:48 -0600

    add routes and html

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -11,23 +11,37 @@ $("#searchButton").click(function(e){
     dataType : "json",
     success : function(parsed_json) {
       console.log(parsed_json);
-     everything = "<ul>";
-    $.each(parsed_json, function(i,item) {
-    //everything += "<li>"+parsed_json[i]['defenition']+"</li>";
-    //   everything +="</li>";
-    // });
-    // everything +="</ul>";
-    // $("#searchResults").html(everything);
-    //   // var location = parsed_json['location']['city'];
-    //   // var temp_string = parsed_json['current_observation']['temperature_string'];
-    //   // var current_weather = parsed_json['current_observation']['weather'];
-    //   // everything = "<ul>";
-    //   // everything += "<li>Location: "+location;
-    //   // everything += "<li>Temperature: "+temp_string;
-    //   // everything += "<li>Weather: "+current_weather;
-    //   // everything += "</ul>";
-    //   // $("#weather").html(everything);
-  });
+     
+      var ndbno = parsed_json['list']['item'][0]['ndbno'];
+      console.log(ndbno);
+
+      myurl ="../number?n=";
+      myurl += ndbno; 
+
+          $.ajax({
+          url : myurl,
+          dataType : "json",
+          success : function(parsed_json2) {
+            console.log(parsed_json2);
+        }  
+      });
+
+  //   $.each(parsed_json['list']['item'], function(i,item) {
+  //   //everything += "<li>"+parsed_json[i]['defenition']+"</li>";
+  //   //   everything +="</li>";
+  //   // });
+  //   // everything +="</ul>";
+  //   // $("#searchResults").html(everything);
+  //   //   // var location = parsed_json['location']['city'];
+  //   //   // var temp_string = parsed_json['current_observation']['temperature_string'];
+  //   //   // var current_weather = parsed_json['current_observation']['weather'];
+  //   //   // everything = "<ul>";
+  //   //   // everything += "<li>Location: "+location;
+  //   //   // everything += "<li>Temperature: "+temp_string;
+  //   //   // everything += "<li>Weather: "+current_weather;
+  //   //   // everything += "</ul>";
+  //   //   // $("#weather").html(everything);
+  // });
     everything += "</ul>";
     $("#searchResults").html(everything);
   }  
